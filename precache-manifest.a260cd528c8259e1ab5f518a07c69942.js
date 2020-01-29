daed4d43779749edcd6988f2400fc944e030178b
dorotakora <dorota.kora@o2.pl>
2020-01-29 09:02:08 +0100

    Updates

---
+++ precache-manifest.a260cd528c8259e1ab5f518a07c69942.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7c5337815ceb7a114b9e0b2d7a92e0",
+    "url": "/recipes_api/index.html"
+  },
+  {
+    "revision": "a3913d3f0b2ef7ba2c58",
+    "url": "/recipes_api/static/css/main.bfc7fecf.chunk.css"
+  },
+  {
+    "revision": "c54655d97be67706c687",
+    "url": "/recipes_api/static/js/2.b5aa9504.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipes_api/static/js/2.b5aa9504.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3913d3f0b2ef7ba2c58",
+    "url": "/recipes_api/static/js/main.3978faae.chunk.js"
+  },
+  {
+    "revision": "446a3d36a1442bc60ed5",
+    "url": "/recipes_api/static/js/runtime-main.b395db29.js"
+  }
+]);
